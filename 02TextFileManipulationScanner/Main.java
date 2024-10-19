beab028eb705fd8f13e42a97c533f4bdaf737f25
wawarnn <akihayakawaaaa@gmail.com>
2024-10-19 13:20:33 +0800

    Main.java

--- 02TextFileManipulationScanner/Main.java
+++ 02TextFileManipulationScanner/Main.java
@@ -1,1 +1,13 @@
+import java.util.Scanner; 
+import java.io.File;
 
+public class Main
+{
+    public static void main(String[] args) throws Exception 
+    	File mamamoFile = new File("wawarnn.txt");
+        Scanner sino = new Scanner(wrFile);
+  
+        while(omsim.hasNextLine())    	
+        	System.out.println(omsim.nextLine());
+    }
+}
