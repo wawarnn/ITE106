abb98047dfb604280af8474a0c4357b0acf444d6
wawarnn <akihayakawaaaa@gmail.com>
2024-10-19 13:19:53 +0800

    Main.java

--- 03TextFileManipulationScannerWithoutLoop/Main.java
+++ 03TextFileManipulationScannerWithoutLoop/Main.java
@@ -1,1 +1,13 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File mamamofile = new File("wawarnn.txt");
+        Scanner sc = new Scanner(wrfile);
+     
+        sc.useDelimiter("\\Z");
+        System.out.println(sc.next());
+    }
+}
