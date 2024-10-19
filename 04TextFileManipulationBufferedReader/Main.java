a2e28b580de1ad0f7acfaf3c31b1b3ca496f6954
wawarnn <akihayakawaaaa@gmail.com>
2024-10-19 13:19:13 +0800

    Main.java

--- 04TextFileManipulationBufferedReader/Main.java
+++ 04TextFileManipulationBufferedReader/Main.java
@@ -1,1 +1,12 @@
+import java.io.*;
 
+public class BufferedReaderTextFile {
+    public static void main(String[] args) throws Exception {
+		File mamamoFile = new File("wawarnn.txt");
+        BufferedReader br = new BufferedReader(new FileReader(wrFile));
+        String st;
+        while((st = br.readLine()) != null) {
+            System.out.println(st);
+        }
+    }
+}
