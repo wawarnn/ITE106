9d0bfa7b6a80de9d8a087a74ef01fb20cd89a868
wawarnn <akihayakawaaaa@gmail.com>
2024-10-19 13:18:15 +0800

    Main.java

--- 05TextFileManipulationFileReader/Main.java
+++ 05TextFileManipulationFileReader/Main.java
@@ -1,1 +1,16 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File mamamoFile = new File("wawarnn.txt");
+        FileReader fr = new FileReader(myFile);
+
+        int i;
+        while ((i = fr.read()) != -1) {
+            System.out.print((char) i);
+        }
+    }
+}
