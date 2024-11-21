ee6b481ef8276f28db20d3ace03d987632bee569
wawarnn <akihayakawaaaa@gmail.com>
2024-11-21 16:20:01 +0800

    Create MenuActionListener.java

---
+++ Menu/act4/MenuActionListener.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+	public class MenuActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(MenuExample.frame,
+				"Got an ActionEvent at" + new Date(e.getwhen())	+ "from" + e.getSource().getclass());
+	}
+}
