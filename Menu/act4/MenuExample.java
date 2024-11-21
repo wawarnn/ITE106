8799e19b26db5c0d32cee0c1d8ff3e2b5bfb1722
wawarnn <akihayakawaaaa@gmail.com>
2024-11-21 16:19:44 +0800

    Create MenuExample.java

---
+++ Menu/act4/MenuExample.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MenuExample {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Menu Example");
+        frame.setSize(220, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(new JMenuItem("New"));
+        fileMenu.add(new JMenuItem("Open"));
+        fileMenu.add(new JMenuItem("Close"));
+
+        JMenu editMenu = new JMenu("Edit");
+        editMenu.add(new JMenuItem("Undo"));
+        editMenu.add(new JMenuItem("Redo"));
+        editMenu.add(new JMenuItem("Cut"));
+		
+		JMenuItem Item = new JMenuItem("Close");
+		item.addActionListener(new MenuActionListener ());
+		fileMenu.add(item);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(fileMenu); 
+        menuBar.add(editMenu);  
+
+        frame.setJMenuBar(menuBar);
+        frame.setVisible(true);
+    }
+}
